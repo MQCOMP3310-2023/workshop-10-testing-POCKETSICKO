2b5d08b963f465ed4897e89dc5d498e8e7f3c00d
Joshua H <joshua.haworth@students.mq.edu.au>
2023-05-08 15:52:31 +1000

    workshop 10 finished

--- project/auth.py
+++ project/auth.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, current_app
 from flask_login import login_user, login_required, logout_user
-from sqlalchemy import text
+from sqlalchemy import text, sql
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
@@ -21,7 +21,7 @@ def login_post():
 
     # check if the user actually exists
     # take the user-supplied password and compare it with the stored password
-    if not user or not (user.password == password):
+    if not user or not check_password_hash(user.password, password):
         flash('Please check your login details and try again.')
         current_app.logger.warning("User login failed")
         return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
@@ -40,14 +40,15 @@ def signup_post():
     name = request.form.get('name')
     password = request.form.get('password')
 
-    user = db.session.execute(text('select * from user where email = "' + email +'"')).all()
+    user = db.session.execute(text('select * from user where email = :email'), {'email': email}).all()
     if len(user) > 0: # if a user is found, we want to redirect back to signup page so user can try again
         flash('Email address already exists')  # 'flash' function stores a message accessible in the template code.
         current_app.logger.debug("User email already exists")
         return redirect(url_for('auth.signup'))
 
     # create a new user with the form data. TODO: Hash the password so the plaintext version isn't saved.
-    new_user = User(email=email, name=name, password=password)
+    hashedpassword = generate_password_hash(password, method='sha256', salt_length = 8)
+    new_user = User(email=email, name=name, password=hashedpassword)
 
     # add the new user to the database
     db.session.add(new_user)
@@ -58,7 +59,7 @@ def signup_post():
 @auth.route('/logout')
 @login_required
 def logout():
-    logout_user();
+    logout_user()
     return redirect(url_for('main.index'))
 
 # See https://www.digitalocean.com/community/tutorials/how-to-add-authentication-to-your-app-with-flask-login for more information
